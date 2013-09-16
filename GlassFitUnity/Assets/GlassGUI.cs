b0c6d588b1c5e54dcf9a0b5254211d821f8b26f7
Ugbot <bengamble7@gmail.com>
2013-09-16 19:10:30 +0100

    more Merging

--- GlassFitUnity/Assets/GlassGUI.cs
+++ GlassFitUnity/Assets/GlassGUI.cs
@@ -15,6 +15,9 @@ public class GlassGUI : MonoBehaviour {
 	private int touchCount = 0;
 	
 	private const int MARGIN = 15;
+	private const int SUBMARGIN = 5;
+	
+	private const float OPACITY = 0.5f;
 	
 	// Left side top
 	private Rect target;	
@@ -32,7 +35,8 @@ public class GlassGUI : MonoBehaviour {
 	private string paceText;
 	// Right side bottom
 	private Rect map;
-	private string mapText;
+	private Rect mapSelf;
+	private Rect mapTarget;
 	
 	// Buttons
 	private Rect start;
@@ -46,18 +50,33 @@ public class GlassGUI : MonoBehaviour {
 	// Debug
 	private Rect debug;
 	private string debugText;
+<<<<<<< HEAD
 			
 	private int originalWidth = 800;  // define here the original resolution
   	private int originalHeight = 500; // you used to create the GUI contents 
  	private Vector3 scale;
 
+=======
+	
+	// Background textures
+	Texture2D normal;
+	Texture2D info;
+	Texture2D warning;
+	
+	// Map textures
+	Texture2D selfIcon;
+	Texture2D targetIcon;
+	Texture2D mapTexture;
+	
+>>>>>>> e4a3fcb4a8ad93a5c74d8a31aadd1dd35e7c502d
 	void Start () {
 		
 
 
 		// Left side top
-		target =   new Rect(MARGIN, MARGIN, 150, 100);	
+		target =   new Rect(MARGIN, MARGIN, 200, 100);	
 		// Left side bottom
+<<<<<<< HEAD
 		distance = new Rect(MARGIN, originalHeight-100, 150, 100);
 		time =     new Rect(MARGIN, distance.y-MARGIN-100, 150, 100);
 		
@@ -66,6 +85,18 @@ public class GlassGUI : MonoBehaviour {
 		pace =     new Rect(originalWidth-MARGIN, calories.y+MARGIN+100, 150, 100);
 		// Right side bottom
 		map =      new Rect(originalWidth-MARGIN, originalHeight-MARGIN-150, 150, 150);
+=======
+		distance = new Rect(MARGIN, Screen.height-MARGIN-100, 200, 100);
+		time =     new Rect(MARGIN, distance.y-SUBMARGIN-100, 200, 100);
+		
+		// Right side top
+		calories = new Rect(Screen.width-MARGIN-200, MARGIN, 200, 100);
+		pace =     new Rect(Screen.width-MARGIN-200, calories.y+SUBMARGIN+100, 200, 100);
+		// Right side bottom
+		map =      new Rect(Screen.width-MARGIN-200, Screen.height-MARGIN-200, 200, 150);
+		mapSelf =  new Rect(0, 0, 30, 30);
+		mapTarget = new Rect(0, 0, 30, 30);
+>>>>>>> e4a3fcb4a8ad93a5c74d8a31aadd1dd35e7c502d
 		
 		// Buttons
 		start =    new Rect((originalWidth)/2, (originalHeight-100)/2-MARGIN, 200, 100);
@@ -75,16 +106,36 @@ public class GlassGUI : MonoBehaviour {
 		gpsLock =  new Rect(originalWidth/2+75, MARGIN, 50, 50);
 		
 		// *** DEBUG
+<<<<<<< HEAD
 		debug =    new Rect(originalWidth/2+50, originalHeight-MARGIN, 100, 100);
 		
 
+=======
+		debug =    new Rect(250, Screen.height-100-MARGIN, Screen.width-250*2, 100);
+>>>>>>> e4a3fcb4a8ad93a5c74d8a31aadd1dd35e7c502d
 		// *** DEBUG
 		
-		targetText = "Behind\n";
 		distanceText = "Distance\n";
 		timeText = "Time\n";
 		caloriesText = "Calories\n";
 		paceText = "Pace/KM\n";	
+		
+		Color white = new Color(0.9f, 0.9f, 0.9f, OPACITY);
+		normal = new Texture2D(1, 1);
+		normal.SetPixel(0,0,white);
+		normal.Apply();
+		Color green = new Color(0f, 0.9f, 0f, OPACITY);
+		info = new Texture2D(1, 1);
+		info.SetPixel(0,0,green);
+		info.Apply();
+		Color red = new Color(0.9f, 0f, 0f, OPACITY);
+		warning = new Texture2D(1, 1);
+		warning.SetPixel(0,0,red);
+		warning.Apply();
+		
+		selfIcon = Resources.Load("Self") as Texture2D;
+		targetIcon = Resources.Load("Target") as Texture2D;
+		mapTexture = Resources.Load("DummyMap") as Texture2D;
 		
 #if UNITY_ANDROID && !UNITY_EDITOR 
 		ji = new Platform();
@@ -120,6 +171,7 @@ public class GlassGUI : MonoBehaviour {
 	
 	void OnGUI ()
 	{
+<<<<<<< HEAD
 		
 		
 		
@@ -129,14 +181,35 @@ public class GlassGUI : MonoBehaviour {
     var svMat = GUI.matrix; // save current matrix
     // substitute matrix - only scale is altered from standard
     GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
+=======
+		GUI.skin.label.fontSize = 15;
+>>>>>>> e4a3fcb4a8ad93a5c74d8a31aadd1dd35e7c502d
 		
 		GUI.skin.box.wordWrap = true;
+		GUI.skin.box.fontSize = 30;
+		GUI.skin.box.fontStyle = FontStyle.Bold;
+		GUI.skin.box.alignment = TextAnchor.MiddleCenter;				
+		GUI.skin.box.normal.background = normal;
+		GUI.skin.box.normal.textColor = Color.black;
 		
-		GUI.Box(gpsLock, "GPS: " + ji.hasLock());
-		GUI.Box(target, targetText+SiDistance( ji.DistanceBehindTarget() ));
-		GUI.Box(distance, distanceText+SiDistance( ji.Distance() ));
-		GUI.Box(time, timeText+Timestamp( ji.Time() ));
+		GUI.Label(gpsLock, "GPS: " + ji.hasLock());
+				
+		GUIStyle targetStyle = new GUIStyle(GUI.skin.box);
+		long targetDistance = ji.DistanceBehindTarget();
+		if (targetDistance > 0) {
+			targetStyle.normal.background = warning; 
+			targetText = "Behind!\n";
+		} else {
+			targetStyle.normal.background = info; 
+			targetText = "Ahead\n";
+		}
+		targetStyle.normal.textColor = Color.white;		
+		GUI.Box(target, targetText+"<i>"+SiDistance( Math.Abs(targetDistance) )+"</i>", targetStyle);
+		long selfDistance = ji.Distance();
+		GUI.Box(distance, distanceText+SiDistance( selfDistance));
+		GUI.Box(time, timeText+TimestampMMSSdd( ji.Time() ));
 		GUI.Box(calories, caloriesText + ji.Calories());
+<<<<<<< HEAD
 		GUI.Box(pace, paceText+Timestamp(speedToKmPace( ji.Pace() )) );
 		GUI.Box(map, "TODO");
 		
@@ -149,6 +222,32 @@ public class GlassGUI : MonoBehaviour {
 			started = true;
 		}
 		
+=======
+		GUI.Box(pace, paceText+TimestampMMSS(speedToKmPace( ji.Pace() )) );
+		
+		// Map
+		// TODO: Stencil out circle
+		Color original = GUI.color;
+		GUI.color = new Color(1f, 1f, 1f, OPACITY);
+				
+		float selfOnMap = selfDistance/map.height;
+		Rect mapCoords = new Rect(0, selfOnMap, 1, selfOnMap+0.3f);
+		GUI.DrawTextureWithTexCoords(map, mapTexture, mapCoords);
+
+		mapSelf.x = map.x + map.width/2 - mapSelf.width/2;
+		mapSelf.y = map.y + map.height/2 - mapSelf.height/2;
+		
+		int targetDistanceOnMap = Convert.ToInt32(targetDistance);
+		int maxDistanceOnMap = Convert.ToInt32(map.height/2);
+		if (targetDistanceOnMap > maxDistanceOnMap) targetDistanceOnMap = maxDistanceOnMap; 
+		if (-targetDistanceOnMap > maxDistanceOnMap) targetDistanceOnMap = -maxDistanceOnMap; 
+		mapTarget.x = map.x + map.width/2 - mapTarget.width/2;
+		mapTarget.y = map.y - targetDistanceOnMap + map.height/2 - mapTarget.height/2;
+		
+		GUI.DrawTexture(mapSelf, selfIcon);
+		GUI.DrawTexture(mapTarget, targetIcon);
+		GUI.color = original;
+>>>>>>> e4a3fcb4a8ad93a5c74d8a31aadd1dd35e7c502d
 		
 		if (!started && GUI.Button (start, startText)) {
 			ji.Start(false);
@@ -161,7 +260,7 @@ public class GlassGUI : MonoBehaviour {
 			started = true;
 		}
 		// *** DEBUG
-		GUI.Box(debug, debugText + ji.DebugLog());
+		GUI.TextArea(debug, debugText + ji.DebugLog());
 		// *** DEBUG
 			GUI.matrix = svMat; // restore matrix
 	
@@ -182,13 +281,18 @@ public class GlassGUI : MonoBehaviour {
 	long speedToKmPace(float speed) {
 		if (speed <= 0) return 0;
 		// m/s -> ms/km
-		return Convert.ToInt64(1000*1/(speed));
+		return Convert.ToInt64(1000*(1/speed));
 	}
 	
-	string Timestamp(long milliseconds) {
+	string TimestampMMSSdd(long milliseconds) {
 		TimeSpan span = TimeSpan.FromMilliseconds(milliseconds);
 
 		return string.Format("{0:00}:{1:00}:{2:00}",span.Minutes,span.Seconds,span.Milliseconds/10);	
+	}
+	string TimestampMMSS(long seconds) {
+		TimeSpan span = TimeSpan.FromSeconds(seconds);
+
+		return string.Format("{0:00}:{1:00}",span.Minutes,span.Seconds);	
 	}
 }
 
