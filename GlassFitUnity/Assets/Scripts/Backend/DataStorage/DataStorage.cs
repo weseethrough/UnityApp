247fc63f3843de6635f2fe2ac46268e78a5b3d91
luiner <luiner@gmail.com>
2013-10-03 15:03:57 +0100

    [ADD] First bits for data manager. Includes string to byte serializer and deserializer.

---
+++ GlassFitUnity/Assets/Scripts/Backend/DataStorage/DataStorage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class DataStorage : MonoBehaviour 
+{
+    //static accees and properties
+	static public DataStorage 	instance;
+	
+    //public properties
+	public string mainBlobName 	= "core";
+
+    //private properties
+    private Dictionary<String, DataStoredType> data;
+	
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("You should have only one datastorage at a time!");
+        }
+        instance = this;
+    }
+
+    public void Initialize(byte[] source)
+    {
+        
+    }
+
+}
---
+++ GlassFitUnity/Assets/Scripts/Backend/DataStorage/DataStoredType.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DataStoredType 
+{
+    public abstract byte[] Serialize();
+    public abstract void DeSerialize(byte[] source);
+}
