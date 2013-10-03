36de6c36b4f3b3207a7567a1c7a90f452a815aa9
luiner <luiner@gmail.com>
2013-10-03 16:11:57 +0100

    [ADD] platform initialziation for data storage

--- GlassFitUnity/Assets/Scripts/Backend/DataStorage/DataStorage.cs
+++ GlassFitUnity/Assets/Scripts/Backend/DataStorage/DataStorage.cs
@@ -8,11 +8,16 @@ public class DataStorage : MonoBehaviour
     //static accees and properties
 	static public DataStorage 	instance;
 	
+	
     //public properties
+	//Note! blob names are used by machine to define file names. Ensure any names used are standard characters for file name.
 	public string mainBlobName 	= "core";
+	public string localizationBlobName 	= "locEn";
 
     //private properties
     private Dictionary<String, DataStoredType> data;
+	private Platform platform;
+	
 	
     void Awake()
     {
@@ -21,11 +26,27 @@ public class DataStorage : MonoBehaviour
             Debug.LogError("You should have only one datastorage at a time!");
         }
         instance = this;
+				
     }
 
     public void Initialize(byte[] source)
     {
-        
+        platform = new Platform();
+		
+		InitializeBlob( platform.LoadBlob(mainBlobName) );
+		InitializeBlob( platform.LoadBlob(localizationBlobName) );
     }
+	
+	private void InitializeBlob(byte[] source)
+	{
+		if (source == null)
+		{
+			Debug.LogError("Blob doesnt exist")	;
+			return;			
+		}
+		
+		
+		
+	}
 
 }
