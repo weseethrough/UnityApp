bd48e260a6fc7c3f9ff0f1527f51762aac951d32
amerigo14 <amerigo14@hotmail.com>
2013-10-15 09:12:42 +0100

    All the metas

--- GlassFitUnity/Assets/David/PBRunnerController.cs
+++ GlassFitUnity/Assets/David/PBRunnerController.cs
@@ -19,14 +19,18 @@ public class PBRunnerController : MonoBehaviour {
 	private int originalWidth = 800;
 	private int originalHeight = 500;
 	
+	private Animator anim; 
+	private float speed;
+	
 	// Use this for initialization
 	void Start () {
 		inputData = new Platform();
-		
+		anim = GetComponent<Animator>();
 		scale.x = (float)Screen.width / originalWidth;
 		scale.y = (float)Screen.height / originalHeight;
     	scale.z = 1;
-
+		anim.speed = inputData.getCurrentSpeed(0) / 2.2f;
+		speed = inputData.getCurrentSpeed(0);
 	}
 	
 	void OnEnable() {
@@ -36,6 +40,19 @@ public class PBRunnerController : MonoBehaviour {
 	void Update () {
 				
 		inputData.Poll();
+		
+		float newSpeed = inputData.getCurrentSpeed(0);
+		if(speed != newSpeed)
+		{
+			speed = newSpeed;
+			if(speed > 2.2f && speed < 4.0f) {
+				anim.speed = newSpeed / 2.2f;
+			} else if(speed > 4.0f) {
+				anim.speed = newSpeed / 4.0f;
+			} else {
+				anim.speed = newSpeed / 1.25f;
+			}
+		}
 		
 		scaledDistance = inputData.DistanceBehindTarget() * 135;
 		Vector3 movement = new Vector3(xDist, yDist,(float)scaledDistance);
