2ca506bd1efc17c1dd89d747af9feea1a9e19128
Ben Lister <ben.lister@cantab.net>
2013-11-13 10:35:06 +0000

    Added lat/long to xyz conversion in Position

--- GlassFitUnity/Assets/javaInterface/Position.cs
+++ GlassFitUnity/Assets/javaInterface/Position.cs
@@ -1,13 +1,21 @@
 using System;
+using UnityEngine;
 
-public class Position
-{
+public class Position {
+	
 	public float latitude { get; set; }
 	public float longitude { get; set; }
+	private float degreesToMetres = 111111.11f;
 	
 	public Position(float latitude, float longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
+	
+	public Vector3 toXYZ() {
+		float x = latitude * degreesToMetres;
+		float y = 0f; //height
+		float z = longitude * degreesToMetres * (float)Math.Cos (latitude);
+		return new Vector3(x,y,z);
+	}
 }
-
