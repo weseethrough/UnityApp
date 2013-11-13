b1a5e724843f8d5e5a6d185472aeb1cec6ea34bb
amerigo14 <amerigo14@hotmail.com>
2013-11-13 17:49:54 +0000

    post-merge fixes

--- GlassFitUnity/Assets/Scripts/TrackPanel.cs
+++ GlassFitUnity/Assets/Scripts/TrackPanel.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
-public class TrackPanel : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+[Serializable]
+public class TrackPanel : Panel {
 	
+	public TrackPanel(SerializationInfo info, StreamingContext ctxt) : base(info, ctxt) {
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public TrackPanel() {}
 	
+	protected override void Initialize()
+    {
+        base.Initialize();
+//		if(Platform.Instance != null) {
+//			Platform.Instance.stopTrack();
+//		}
+	}
+	
+	public override void OnClick(FlowButton button)
+	{
+		base.OnClick(button);
+		
+		switch(button.name)
+		{
+			case "NextButton":
+				Platform.Instance.getNextTrack();
+				break;
+				
+			case "PrevButton":
+				Platform.Instance.getPreviousTrack();
+				break;
+				
+			case "SetTrackButton":
+				Platform.Instance.setTrack();
+				break;
+
+			case "BackSettingsButton":
+				GameObject.Find("TrackSelect").renderer.enabled = false;
+				break;		
+		}
 	}
 }
