e42934701c93b2c41a3e7bcc958d77286a06e2bc
amerigo14 <amerigo14@hotmail.com>
2013-12-04 14:39:36 +0000

    Jan's UI stuff

---
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/LoadingCont.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingCont : MonoBehaviour {
+	
+	private float rotate = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotate += 10 * Time.deltaTime;
+		
+//		FlowState fs = FlowStateMachine.GetCurrentFlowState();
+//		GConnector gConect = fs.Outputs.Find(r => r.Name == "completeExit");
+//		if(gConect != null) {
+//			fs.parentMachine.FollowConnection(gConect);
+//		} else {
+//			UnityEngine.Debug.Log("Game: No connection found!");
+//		}
+		
+		transform.rotation = Quaternion.Euler(0, 0, rotate);
+	}
+}
