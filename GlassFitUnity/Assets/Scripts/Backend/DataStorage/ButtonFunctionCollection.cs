c9340f6ab4f55acba02733d0078e4e5d5ffad61d
husberg <janne@husberg.fi>
2013-12-04 13:18:41 +0000

    Fix thread creation race condition.

--- GlassFitUnity/Assets/Scripts/Backend/DataStorage/ButtonFunctionCollection.cs
+++ GlassFitUnity/Assets/Scripts/Backend/DataStorage/ButtonFunctionCollection.cs
@@ -474,74 +474,79 @@ public class ButtonFunctionCollection
 		shandler = new Platform.OnSync(() => {
 			Platform.Instance.onSync -= shandler;
 			
-			Thread loaderThread = new Thread(() => {
+			lock(DataVault.data) {
+				if (DataVault.Get("loaderthread") != null) return;
+				Thread loaderThread = new Thread(() => {
 #if !UNITY_EDITOR
-				AndroidJNI.AttachCurrentThread();
+					AndroidJNI.AttachCurrentThread();
 #endif				
-				try {
-					// Reset world
-					Platform.Instance.ResetTargets();
-					DataVault.Remove("challenges");
-					DataVault.Remove("finish");
-					
-					List<Challenge> relevant = new List<Challenge>();		
-					int? finish = null;
-					
-					Notification[] notifications = Platform.Instance.Notifications();
-					foreach (Notification notification in notifications) {
-						if (notification.read) continue;
-						if (string.Equals(notification.node["type"], "challenge")) {
-							int challengerId = notification.node["from"].AsInt;
-							if (challengerId == null) continue;
-							string challengeId = notification.node["challenge_id"].ToString();
-							if (challengeId == null || challengeId.Length == 0) continue;
-							if (challengeId.Contains("$oid")) challengeId = notification.node["challenge_id"]["$oid"].ToString();
-							challengeId = challengeId.Replace("\"", "");
-							
-							Debug.Log("AcceptChallenges: " + challengeId + " from " + challengerId);
-							
-							Platform.Instance.message = "Please wait while we fetch challenge"; 
-							Challenge potential = Platform.Instance.FetchChallenge(challengeId);
-							if (potential == null) continue;
-							if (potential is DistanceChallenge) {
-								DistanceChallenge challenge = potential as DistanceChallenge;					
+					try {
+						// Reset world
+						Platform.Instance.ResetTargets();
+						DataVault.Remove("challenges");
+						DataVault.Remove("finish");
+						
+						List<Challenge> relevant = new List<Challenge>();		
+						int? finish = null;
+						
+						Notification[] notifications = Platform.Instance.Notifications();
+						foreach (Notification notification in notifications) {
+							if (notification.read) continue;
+							if (string.Equals(notification.node["type"], "challenge")) {
+								int challengerId = notification.node["from"].AsInt;
+								if (challengerId == null) continue;
+								string challengeId = notification.node["challenge_id"].ToString();
+								if (challengeId == null || challengeId.Length == 0) continue;
+								if (challengeId.Contains("$oid")) challengeId = notification.node["challenge_id"]["$oid"].ToString();
+								challengeId = challengeId.Replace("\"", "");
 								
-								Platform.Instance.message = "Please wait while we fetch track"; 
-								Track track = challenge.UserTrack(challengerId);
-								if (track != null) {
-									Platform.Instance.FetchTrack(track.deviceId, track.trackId); // Make sure we have the track in the local db
-									TargetTracker tracker = Platform.Instance.CreateTargetTracker(track.deviceId, track.trackId);
-									User challenger = Platform.Instance.GetUser(challengerId);
-									tracker.name = challenger.username;
-									if (tracker.name == null || tracker.name.Length == 0) tracker.name = challenger.name;
-								} // else race leader/friends/creator?
-			
-								relevant.Add(challenge); 					
-								if (!finish.HasValue || finish.Value < challenge.distance) finish = challenge.distance;					
+								Debug.Log("AcceptChallenges: " + challengeId + " from " + challengerId);
+								
+								Platform.Instance.message = "Please wait while we fetch challenge"; 
+								Challenge potential = Platform.Instance.FetchChallenge(challengeId);
+								if (potential == null) continue;
+								if (potential is DistanceChallenge) {
+									DistanceChallenge challenge = potential as DistanceChallenge;					
+									
+									Platform.Instance.message = "Please wait while we fetch track"; 
+									Track track = challenge.UserTrack(challengerId);
+									if (track != null) {
+										Platform.Instance.FetchTrack(track.deviceId, track.trackId); // Make sure we have the track in the local db
+										TargetTracker tracker = Platform.Instance.CreateTargetTracker(track.deviceId, track.trackId);
+										User challenger = Platform.Instance.GetUser(challengerId);
+										tracker.name = challenger.username;
+										if (tracker.name == null || tracker.name.Length == 0) tracker.name = challenger.name;
+									} // else race leader/friends/creator?
+				
+									relevant.Add(challenge); 					
+									if (!finish.HasValue || finish.Value < challenge.distance) finish = challenge.distance;					
+								}
 							}
+						}		
+						if (!finish.HasValue || relevant.Count == 0) {
+							Platform.Instance.message = "No relevant challenges";
+							return;
 						}
-					}		
-					if (!finish.HasValue || relevant.Count == 0) {
-						Platform.Instance.message = "No relevant challenges";
-						return;
-					}
-					
-					Debug.Log("AcceptChallenges: Accepted " + relevant.Count + " challenges");
-					Platform.Instance.message = "Accepted " + relevant.Count + " challenges";
-					DataVault.Set("challenges", relevant);
-					DataVault.Set("finish", finish.Value);
-					
-					AutoFade.LoadLevel(1, 0.1f, 1.0f, Color.black);
-					
-					// Follow connection since the function may have been called asynchronously
-					panel.parentMachine.FollowConnection(gConect);
-				} finally {
+						
+						Debug.Log("AcceptChallenges: Accepted " + relevant.Count + " challenges");
+						Platform.Instance.message = "Accepted " + relevant.Count + " challenges";
+						DataVault.Set("challenges", relevant);
+						DataVault.Set("finish", finish.Value);
+						
+						AutoFade.LoadLevel(1, 0.1f, 1.0f, Color.black);
+						
+						// Follow connection since the function may have been called asynchronously
+						panel.parentMachine.FollowConnection(gConect);
+					} finally {
+						DataVault.Remove("loaderthread");
 #if !UNITY_EDITOR
-					AndroidJNI.DetachCurrentThread();
+						AndroidJNI.DetachCurrentThread();
 #endif					
-				}
-			});
-			loaderThread.Start();
+					}
+				});
+				DataVault.Set("loaderthread", loaderThread);
+				loaderThread.Start();
+			}
 		});
 		Platform.Instance.onSync += shandler;
 		
