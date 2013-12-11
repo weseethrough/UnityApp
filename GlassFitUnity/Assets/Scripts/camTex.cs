00389695707c7f6de05ec00688d6ad666af3ec4a
amerigo14 <amerigo14@hotmail.com>
2013-12-11 15:05:43 +0000

    Small adjustment to camera, framerate slightly improved

--- GlassFitUnity/Assets/Scripts/camTex.cs
+++ GlassFitUnity/Assets/Scripts/camTex.cs
@@ -5,13 +5,10 @@ public class camTex : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	string DevName = WebCamTexture.devices[0].name;
-	
-		
-		
-	WebCamTexture	webCamTexture = new WebCamTexture(DevName, 320, 200, 30);
-	webCamTexture.Play();
-	renderer.material.mainTexture = webCamTexture;
+		string DevName = WebCamTexture.devices[0].name;			
+		WebCamTexture	webCamTexture = new WebCamTexture(DevName, 320, 200, 30);
+		webCamTexture.Play();
+		renderer.material.mainTexture = webCamTexture;
 	}
 	
 	// Update is called once per frame
