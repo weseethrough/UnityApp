196d728e085a9098fae8db7a1dd905c6ea64d751
Andy Haith <andyhaith@gmail.com>
2014-01-09 08:30:31 +0000

    Added paging indicator to First Race

---
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/PageIndicatorController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PageIndicatorController : MonoBehaviour {
+	protected int currentPage = 0;
+	protected int numPages = 1;
+	
+	float xSpacing = 60.0f;
+	float indicatorHeight = 0.0f;
+	
+	GameObject spriteOn;
+	
+	// Use this for initialization
+	void Start () {
+		//load number of pages from datavault
+		try {
+				numPages = (int)DataVault.Get("NumberOfPages");
+		} catch (Exception e) {
+			UnityEngine.Debug.LogWarning("PageIndicator: couldn't retrieve number of pages from datavault");
+			numPages = 5;
+		}
+	
+		spriteOn = GameObject.Find("PageIndicator_on");
+		if(spriteOn)
+		{
+			indicatorHeight = spriteOn.transform.localPosition.y;
+		}
+		else { UnityEngine.Debug.Log("PageIndicator: couldn't find on widget"); }
+		
+		//clone off element as needed and position
+		for(int i=0; i<numPages; i++)
+		{
+			GameObject spriteOffi = GameObject.Find("PageIndicator_off" + i);
+			//calculate position
+			Vector2 pos = new Vector2(PosForIndex(i), indicatorHeight);
+			spriteOffi.transform.localPosition = pos;
+		}
+		
+		//initialise current page
+		DataVault.Set("currentPageIndex",0);
+		
+		UpdateCurrentPage();
+		
+	}
+	
+	protected float PosForIndex(int i)
+	{
+		float parametricPos = i- (numPages -1) * 0.5f;
+		return parametricPos * xSpacing;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//update current page
+		UpdateCurrentPage();
+	}
+	
+	protected void UpdateCurrentPage() {
+		//look up current page in datavault
+		try {
+			currentPage = (int)DataVault.Get("currentPageIndex");
+		} catch( Exception e ) {
+			UnityEngine.Debug.Log("PageIndicator: couldn't retrieve current page from datavault");
+			currentPage = 0;
+		}
+			
+
+		//move selected indicator to position
+		spriteOn = GameObject.Find("PageIndicator_on");
+		spriteOn.transform.localPosition = new Vector2(PosForIndex(currentPage), indicatorHeight);
+	}
+	
+	public void GhostProgressBeyondPage(int lastAllowedPage)
+	{
+		for(int i=lastAllowedPage+1; i<numPages; i++)
+		{
+			GameObject indicator = GameObject.Find("PageIndicator_off" + i);
+			UISprite sprite = indicator.GetComponent("UISprite") as UISprite;
+			sprite.color = new Color(0.9f, 0.9f, 0.9f);
+		}
+	}
+	
+	public void UnGhostAllPages()
+	{
+		for(int i=0; i<numPages; i++)
+		{
+			GameObject indicator = GameObject.Find("PageIndicator_off" + i);
+			UISprite sprite = indicator.GetComponent("UISprite") as UISprite;
+			sprite.color = UnityEngine.Color.black;
+		}
+	}
+	
+}
--- GlassFitUnity/Assets/Scripts/UI/UI Controller/SwipeListener.cs
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/SwipeListener.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SwipeListener : MonoBehaviour {
 	
@@ -26,25 +27,60 @@ public class SwipeListener : MonoBehaviour {
 		
 	protected virtual void handleLeft()
 	{
-		FollowFlowLinkNamed("Back");
+		if(FollowFlowLinkNamed("Back"))
+		{
+			int currentPage = 1;
+			//increment page
+			try {
+				currentPage = (int)DataVault.Get("currentPageIndex");
+			} catch (Exception e) {
+				UnityEngine.Debug.Log("Couldn't get current page index");
+				UnityEngine.Debug.LogException(e);
+			}
+			try {
+				DataVault.Set("currentPageIndex", currentPage - 1);
+			} catch (Exception e) {
+				UnityEngine.Debug.Log("Couldn't set current page index");
+				UnityEngine.Debug.LogException(e);
+			}
+			
+		}
 	}
 	
 	protected virtual void handleRight()
 	{
-		FollowFlowLinkNamed("Swipe");
+		if(FollowFlowLinkNamed("Swipe"))
+		{
+			int currentPage = 1;
+			//decrement page
+			try {
+				currentPage = (int)DataVault.Get("currentPageIndex");
+			} catch (Exception e) {
+				UnityEngine.Debug.LogWarning("Couldn't get current page index");
+				UnityEngine.Debug.LogException(e);
+			}
+			try {
+				DataVault.Set("currentPageIndex", currentPage + 1);
+			} catch (Exception e) {
+				UnityEngine.Debug.LogWarning("Couldn't set current page index");
+				UnityEngine.Debug.LogException(e);
+			}
+		}
 	}
 	
-	protected void FollowFlowLinkNamed(string name)
+	protected bool FollowFlowLinkNamed(string name)
 	{
 		FlowState fs = FlowStateMachine.GetCurrentFlowState();
 		GConnector gConnect = fs.Outputs.Find( r => r.Name == name );
 		if(gConnect != null)
 		{
 			fs.parentMachine.FollowConnection(gConnect);
+			return true;
 		}
 		else
 		{
 			UnityEngine.Debug.Log("swipelistener: didn't find flow link named :" + name);
+			return false;
 		}
 	}
 	
