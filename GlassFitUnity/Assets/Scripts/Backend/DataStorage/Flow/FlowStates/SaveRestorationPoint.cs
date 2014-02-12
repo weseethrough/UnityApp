866a0e622616be3b7f850cba7f4411ddd40a10d4
Robert <luiner@gmail.com>
2014-02-12 12:18:30 +0000

    Restoration Beta test

---
+++ GlassFitUnity/Assets/Scripts/Backend/DataStorage/Flow/FlowStates/SaveRestorationPoint.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+/// <summary>
+/// exit state which is simply blank end. It doesn't have any navigation out connections.
+/// </summary>
+[Serializable]
+public class SaveRestorationPoint : FlowState 
+{
+	/// <summary>
+	/// default constructor
+	/// </summary>	
+	public SaveRestorationPoint() : base() {}
+
+    /// <summary>
+    /// deserialziation constructor
+    /// </summary>
+    /// <param name="info">seirilization info conataining class data</param>
+    /// <param name="ctxt">serialization context </param>
+    /// <returns></returns>
+    public SaveRestorationPoint(SerializationInfo info, StreamingContext ctxt)
+        : base(info, ctxt)
+	{
+    }
+
+    /// <summary>
+    /// serialization function called by serializer
+    /// </summary>
+    /// <param name="info">serialziation info where all data would be pushed to</param>
+    /// <param name="ctxt">serialzation context</param>
+    /// <returns></returns>
+    public override void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+   	{
+        base.GetObjectData(info, ctxt);
+   	}
+
+    /// <summary>
+    /// Gets display name of the node, helps with node identification in editor
+    /// </summary>
+    /// <returns>name of the node</returns>
+    public override string GetDisplayName()
+    {
+        base.GetDisplayName();
+
+        return "Save Restoration Point";
+    }
+
+    /// <summary>
+    /// initialzies node and creates name for it. Makes as well default iput/output connection sockets
+    /// </summary>
+    /// <returns></returns>
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        Size = new Vector2(175, 80);        
+        NewInput("In", "Flow");
+        NewOutput("Out", "Flow");
+        NewParameter("Name", GraphValueType.String, "Save Restoration Point");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override void Entered()
+    {
+        base.Entered();
+        DataStore.LoadStorage(DataStore.BlobNames.saves);
+
+        Storage storage = DataStore.GetStorage(DataStore.BlobNames.saves);
+        
+        GameStateRestorable gsRestorable = storage != null ? storage.dictionary.Get("Save1") as GameStateRestorable : null;
+        if (gsRestorable != null && gsRestorable.GoToRestoredState())
+        {
+            
+        }
+        else if (Outputs.Count > 0 && parentMachine != null)
+        {
+            parentMachine.FollowConnection(Outputs[0]);
+        }
+        else
+        {
+            Debug.LogError("Dead end start");
+        }
+
+    }
+}
