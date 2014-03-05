0a8b5fbf4a22ad832fd69bc608e15f5370ff03a9
amerigo14 <amerigo14@hotmail.com>
2014-03-05 17:27:10 +0000

    Dinosaur Snack

--- GlassFitUnity/Assets/Scripts/Game Scripts/Games/Snacks/DinosaurSnack.cs
+++ GlassFitUnity/Assets/Scripts/Game Scripts/Games/Snacks/DinosaurSnack.cs
@@ -1,76 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BoulderSnack : SnackBase {
+public class DinosaurSnack : SnackBase {
 	
-	// The boulder
-	BoulderController boulder;
+	// The dinosaur
+	DinosaurController dinosaur;
 	
-	GameObject mainCamera;
-	
-	public GameObject boulderCamera;
-	
-	/// <summary>
-	/// Override base class. Will enable the boulder to start the game.
-	/// </summary>
 	public override void Begin ()
 	{
-		// Call the base function
 		base.Begin ();
-
 		
 		SetTrack(false);
 		
-		// Enable the boulder if it is there
-		if(boulder != null)
+		if(dinosaur != null)
 		{
-			boulder.enabled = true;
-			UnityEngine.Debug.Log("BoulderSnack: Starting game");
+			dinosaur.enabled = true;
 		}
 		else
 		{
-			UnityEngine.Debug.Log("BoulderSnack: can't enable boulder - is null!"); 
+			UnityEngine.Debug.Log("DinosaurSnack: Can't enable dinosaur - is null!");
 		}
 	}
 	
 	// Use this for initialization
 	void Start () {
-		// Call the base function
+		
 		base.Start();
-		// Find the boulder
-		boulder = GetComponent<BoulderController>();
-		UnityEngine.Debug.Log("BoulderSnack: Start");
+		
+		dinosaur = GetComponent<DinosaurController>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Call the base function
 		base.Update();
 		
-		// Update the display
-		UpdateAhead(boulder.GetDistanceBehindTarget());
+		UpdateAhead(dinosaur.GetDistanceBehindTarget());
 		
-		// Check if the boulder is null
-		if(boulder != null)
+		if(dinosaur != null)
 		{
-			// Check if the boulder reached the player
-			if(boulder.GetDistanceBehindTarget() > 0.0 && !finish)
+			if(dinosaur.GetDistanceBehindTarget() > 0.0 && !finish)
 			{				
 				// Set the attributes for the banner
 				DataVault.Set("death_colour", "EA0000FF");
-				DataVault.Set("snack_result", "You survived for " + SiDistance(boulder.GetPlayerDistanceTravelled()));
+				DataVault.Set("snack_result", "You survived for " + SiDistance(dinosaur.GetPlayerDistanceTravelled()));
 				DataVault.Set("snack_result_desc", "returning to game...");
 				// End the game
 				finish = true;
 				
-//				if(mainCamera != null)
-//				{
-//					mainCamera.SetActive(true);
-//				}
-//				
-//				if(boulderCamera != null) {
-//					boulderCamera.SetActive(false);
-//				}
+				dinosaur.enabled = false;
+				transform.position = new Vector3(0, 0, -1);
 				
 				// Show the banner
 				StartCoroutine(ShowBanner(3.0f));
