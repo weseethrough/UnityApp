9225c3a9ef4ecc30fdd399cc9197cbb0bea8a68d
amerigo14 <amerigo14@hotmail.com>
2014-04-17 16:30:05 +0100

    Added file for beta testing single game

--- GlassFitUnity/Assets/Scripts/UI/UI Controller/DeviceControl.cs
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/DeviceControl.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+using RaceYourself.Models;
 
 public class DeviceControl : MonoBehaviour {
-
+	
 	private float pollTime = 0.0f;
 	
 	// Use this for initialization
@@ -26,7 +29,43 @@ public class DeviceControl : MonoBehaviour {
 		{
 			UnityEngine.Debug.Log("DeviceControl: device obtained");
 			FlowState fs = FlowStateMachine.GetCurrentFlowState();
-			GConnector gConnect = fs.Outputs[0];
+			GConnector gConnect;
+			List<Game> gamesList = Platform.Instance.GetGames();
+			int gameCount = 0;
+			Game chosenGame = null;
+			for(int i=0; i<gamesList.Count; i++) {
+				if(gamesList[i].type != "N/A") {
+					gameCount++;
+					chosenGame = gamesList[i];
+				}
+			}
+			if(gameCount == 1) {
+				DataVault.Set("is_testing", true);
+				gConnect = fs.Outputs.Find(r => r.Name == "GameExit");
+				switch(chosenGame.type) {
+				case "Snack":
+					DataVault.Set ("race_type", "snack");
+					DataVault.Set("finish", 1000);
+					break;
+				case "Race":
+					DataVault.Set ("race_type", "race");
+					DataVault.Set("finish", 1000);
+					break;
+				case "Challenge":
+					DataVault.Set ("race_type", "challenge");
+					gConnect = fs.Outputs.Find(r => r.Name == "ChallengeExit");
+					break;
+				default:
+					UnityEngine.Debug.Log("DeviceControl: game type not recognise - it says " + chosenGame.type);
+					DataVault.Set("is_testing", false);
+					gConnect = fs.Outputs.Find(r => r.Name == "MenuExit");
+					break;
+				}
+			} else {
+				DataVault.Set("is_testing", false);
+				gConnect = fs.Outputs.Find(r => r.Name == "MenuExit");
+			}
+			
 			if(gConnect != null)
 			{
 				UnityEngine.Debug.Log("DeviceControl: connection found, travelling");
@@ -71,33 +110,33 @@ public class DeviceControl : MonoBehaviour {
 		
 		if(pollTime > 5.0f) 
 		{
-#if UNITY_EDITOR
-
-            pollTime -= 500.0f; //simply shouldn't have to happen again
-            FlowState fs = FlowStateMachine.GetCurrentFlowState();
-			FlowState.FollowFlowLinkNamed("MenuExit");
-#else
+			if(Application.isEditor)
+			{
+				pollTime -= 500.0f; //simply shouldn't have to happen again
+				FlowState fs = FlowStateMachine.GetCurrentFlowState();
+				FlowState.FollowFlowLinkNamed("MenuExit");
+			}
 			pollTime -= 5.0f;
 			
 			UpdateStatus();
-//			if(Platform.Instance.Device() != null)
-//			{
-//				UnityEngine.Debug.Log("DeviceControl: device obtained");
-//				FlowState fs = FlowStateMachine.GetCurrentFlowState();
-//				GConnector gConnect = fs.Outputs.Find(r => r.Name == "MenuExit");
-//				if(gConnect != null)
-//				{
-//					UnityEngine.Debug.Log("DeviceControl: connection found, travelling");
-//					fs.parentMachine.FollowConnection(gConnect);
-//				} else
-//				{
-//					UnityEngine.Debug.Log("DeviceControl: connection not found: MenuExit");
-//				}
-//			} else
-//			{
-//				UnityEngine.Debug.Log("DeviceControl: device null");
-//			}
-#endif
-        }
+			//			if(Platform.Instance.Device() != null)
+			//			{
+			//				UnityEngine.Debug.Log("DeviceControl: device obtained");
+			//				FlowState fs = FlowStateMachine.GetCurrentFlowState();
+			//				GConnector gConnect = fs.Outputs.Find(r => r.Name == "MenuExit");
+			//				if(gConnect != null)
+			//				{
+			//					UnityEngine.Debug.Log("DeviceControl: connection found, travelling");
+			//					fs.parentMachine.FollowConnection(gConnect);
+			//				} else
+			//				{
+			//					UnityEngine.Debug.Log("DeviceControl: connection not found: MenuExit");
+			//				}
+			//			} else
+			//			{
+			//				UnityEngine.Debug.Log("DeviceControl: device null");
+			//			}
+			
+		}
 	}
 }
