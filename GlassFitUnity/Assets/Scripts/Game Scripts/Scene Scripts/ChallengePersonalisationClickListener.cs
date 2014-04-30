0d38460c034ece911ffa9551cd84675e93875ad7
Duncan Roberts <dliroberts@gmail.com>
2014-04-30 16:44:54 +0100

    like/dislike now clickeable

---
+++ GlassFitUnity/Assets/Scripts/Game Scripts/Scene Scripts/ChallengePersonalisationClickListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChallengePersonalisationClickListener : MonoBehaviour {
+    private static Log log = new Log("ChallengePersonalisationClickListener");
+
+    void OnClick()
+    {
+        ChallengeControllerPanel panel = FlowStateMachine.GetCurrentFlowState() as ChallengeControllerPanel;
+        if (gameObject.name == "DislikeIcon")
+            panel.DislikeChallenge();
+        else if (gameObject.name == "LikeIcon")
+            panel.LikeChallenge();
+        else
+            log.error("Cannot determine whether the user clicked on 'Dislike' or 'Like'. Code depends on name " +
+                "of GameObjects - should be 'DislikeIcon' and 'LikeIcon'.");
+    }
+}
