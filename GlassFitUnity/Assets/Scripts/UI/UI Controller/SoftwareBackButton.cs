4a59f29a81c94c8dba7dedacc93c7c01c3bff64a
Duncan Roberts <dliroberts@gmail.com>
2014-05-01 17:35:43 +0100

    fixed back button - now able to disable per-screen

---
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/SoftwareBackButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoftwareBackButton : UIComponentSettings {
+
+    void Awake () {
+        DataVault.RegisterListner(this, "transition_ended");
+        Apply ();
+	}
+
+    public override void Apply()
+    {
+        if(Platform.Instance.ProvidesBackButton() || !FlowStateMachine.GetCurrentFlowState().parentMachine.IsBackAllowed())
+        {
+            UnityEngine.Debug.Log("SoftwareBackButton: disabling software back button");
+            gameObject.SetActive(false);
+        }
+        else if (!((bool) DataVault.Get("transition_ended")))
+        {
+            UnityEngine.Debug.Log("SoftwareBackButton: enabling software back button");
+            gameObject.SetActive(true);
+        }
+    }
+
+	public void OnClick ()
+    {
+		//treat as a back gesture
+		GestureHelper.onBack();
+	}
+}
