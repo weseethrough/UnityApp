5595eb1fcbb77165877388b8d1520f9b8a34158c
Ben Lister <ben.lister@cantab.net>
2014-06-02 22:09:32 +0100

    Revert "Smoother list scrolling when dragging"
    This reverts commit 0cc724f580270838c0085fa29c3b73e787e2ee60.

--- GlassFitUnity/Assets/Scripts/UI/UI Controller/LabelScroller.cs
+++ GlassFitUnity/Assets/Scripts/UI/UI Controller/LabelScroller.cs
@@ -69,14 +69,7 @@ public class LabelScroller : MonoBehaviour
 			position.y = labelOffset-margin;
 			velocity = margin;
 		}
-        if(Platform.Instance.GetTouchCount() >= 1)
-        {
-            Vector2? touch = Platform.Instance.GetTouchInput();
-            transform.position = (touch.Value - draggingStartPos.Value);
-        }
-        else{
-            transform.position = position;
-        }
+		transform.position = position;
 	}
 	
 }
