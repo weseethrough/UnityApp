2371e2762e1d832b30e84c531b0c86c5aea91485
Duncan Roberts <dliroberts@gmail.com>
2014-06-09 18:35:51 +0100

    Can now log in by pressing enter from password field in login form.

---
+++ GlassFitUnity/Assets/Scripts/UI/MobileLoginSubmit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileLoginSubmit : MonoBehaviour {
+
+    public void OnSubmit ()
+    {
+        FlowState fs = FlowStateMachine.GetCurrentFlowState();
+        Panel panel = (Panel) fs;
+        GConnector gc = panel.Outputs.Find(r => r.Name == "Login");
+        panel.parentMachine.FollowConnection(gc);
+        ButtonFunctionCollection.AllowLogin(null, fs);
+    }
+}
