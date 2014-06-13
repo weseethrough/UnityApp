5ebf998a8320e4b09e2be68bc339193711eee4c4
Janne Husberg <janne@husberg.fi>
2014-06-13 13:53:37 +0100

    Viz tweaks

---
+++ GlassFitUnity/Assets/Scripts/Utils/Palette.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class Palette
+{
+	public static readonly Color green = new Color(57/255.0f, 188/255.0f, 60/255.0f);
+	public static readonly Color red = new Color(255/255.0f, 0/255.0f, 51/255.0f);
+	public static readonly Color grey = new Color(204/255.0f, 205/255.0f, 204/255.0f);
+	public static readonly Color yellow = new Color(255/255.0f, 204/255.0f, 0/255.0f);
+}
+
