bfffed11b925365ccc7dfd6e49222bd7042a8193
Duncan Roberts <dliroberts@gmail.com>
2014-06-18 20:35:40 +0100

    whitespace change

--- GlassFitUnity/Assets/Scripts/Game Scripts/Games/RaceGame.cs
+++ GlassFitUnity/Assets/Scripts/Game Scripts/Games/RaceGame.cs
@@ -57,7 +57,6 @@ public class RaceGame : GameBase {
             break;
         }
 
-
         Platform.Instance.ResetTargets();
 
         ActorVisualsFactory visualsFactory = gameObject.GetComponent<ActorVisualsFactory>();
