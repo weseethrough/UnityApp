bdefcb5ecc2f2c12bdfc4b1e0033948805c1a47e
Duncan Roberts <dliroberts@gmail.com>
2014-06-24 19:48:40 +0100

    home button only visible on challenge info panel if no back button.

---
+++ GlassFitUnity/Assets/HideHomeIfNavHistory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideHomeIfNavHistory : MonoBehaviour {
+
+	/// <summary>
+    /// Should never have both home and back buttons.
+    /// </summary>
+	void Start () {
+        bool backAllowed = FlowStateMachine.GetCurrentFlowState().parentMachine.IsBackAllowed();
+        gameObject.GetComponentInChildren<UISprite>().enabled = !backAllowed;
+	}
+}
